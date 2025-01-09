9870fbf4080e75cf35a41e00c684013f7f7d8543
Андреев Андрей <andreyan@playrix.com>
2025-01-09 17:58:26 +0300

    - Deserialization for Entities/Components via SimpleJSON

--- Assets/Scripts/LevelContentLoader.cs
+++ Assets/Scripts/LevelContentLoader.cs
@@ -11,13 +11,27 @@ public class LevelContentLoader
 
     public void LoadLevel(string levelName, byte[] ds1Content, string jsonContent)
     {
-        preset = JsonUtility.FromJson<LevelPreset>(jsonContent);
-        old_content = ds1Content;
+        LoadJsonPreset(jsonContent);
+        LoadDS1Content(ds1Content);
+
         Debug.Log("Level loaded " + levelName);
         Debug.Log("Preset name is " + preset.biomeFilename);
         Debug.Log("Ds1 content length " + old_content.Length);
 
 
+    }
+
+    private void LoadJsonPreset(string jsonContent)
+    {
+        preset = JsonUtility.FromJson<LevelPreset>(jsonContent);
+        preset.CreateComponents(jsonContent);
+    }
+
+
+
+    private void LoadDS1Content(byte[] ds1Content)
+    {
+        old_content = ds1Content;
     }
 
     public bool TestLevelLoading(byte[] ds1Content, string jsonContent)
--- Assets/Scripts/LevelPreset.cs
+++ Assets/Scripts/LevelPreset.cs
@@ -1,8 +1,9 @@
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Numerics;
+using SimpleJSON;
+using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 [Serializable]
 public class LevelPresetDependency
@@ -36,16 +37,37 @@ public class LevelEntityComponent
 {
     public string type;
     public string name;
+
+    public virtual void Deserialize(JSONNode json)
+    {
+        string json_text = json.ToString();
+        JsonUtility.FromJsonOverwrite(json_text, this);
+    }
+
+}
+
+[Serializable]
+public class LevelEntityUnknown: LevelEntityComponent
+{
+
+}
+
+public class TransformDefinitionComponent : LevelEntityComponent
+{
+    public UnityEngine.Vector3 position;
+    public UnityEngine.Quaternion orientation;
+    public UnityEngine.Vector3 scale;
+    public bool inheritOnlyPosition = true;
 }
 
 
-[Serializable]
+
 public class LevelEntity
 {
     public string type;
     public string name;
     public Int64 id;
-    public LevelEntityComponent[] components;
+    public List<LevelEntityComponent> components;
 
 }
 
@@ -59,6 +81,52 @@ public class LevelPreset
     public LevelPresetDependencies dependencies;
     public string type;
     public string name;
+    public List<LevelEntity> entities;
+    public LevelEntity terrain;
     public string biomeFilename;
     public string[] perTileBiomeOverrides;
+
+    private LevelEntityComponent CreateComponentByType(string type)
+    {
+        if (type == "TransformDefinitionComponent")
+        {
+            return new TransformDefinitionComponent();
+        }
+        return new LevelEntityUnknown();
+    }
+
+    public void CreateComponents(string jsonContent)
+    {
+        JSONNode root = JSON.Parse(jsonContent);
+        JSONNode json_entities = root["entities"];
+        this.entities = new List<LevelEntity>();
+        foreach (JSONNode entity in json_entities)
+        {
+            LevelEntity new_entity = LoadEntity(entity);
+            this.entities.Add(new_entity);
+        }
+
+        JSONNode json_terrain = root["terrain"];
+        this.terrain = LoadEntity(json_terrain);
+    }
+
+    private LevelEntity LoadEntity(JSONNode entity_node)
+    {
+        LevelEntity new_entity = new LevelEntity();
+        new_entity.type = entity_node["type"];
+        new_entity.name = entity_node["name"];
+        new_entity.id = entity_node["id"];
+
+        JSONNode components = entity_node["components"];
+        new_entity.components = new List<LevelEntityComponent>();
+        foreach (JSONNode component in components)
+        {
+            var component_type = component["type"];
+            var obj = CreateComponentByType(component_type);
+            obj.Deserialize(component);
+            new_entity.components.Add(obj);
+        }
+        return new_entity;
+    }
+
 }
